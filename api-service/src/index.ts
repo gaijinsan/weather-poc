7b3c61d5bdf5c47741b9fb66b7beaca093b58b13
Sean O'Hagan <sohagan@gmail.com>
2025-10-09 22:24:08 +0900

    changed importing method

--- api-service/src/index.ts
+++ api-service/src/index.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import type { Request, Response } from 'express';
 import fs from 'fs';
 
 const app = express();
